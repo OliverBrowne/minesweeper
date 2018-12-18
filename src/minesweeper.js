cee573217152f3fb95897e6087890fb54fee7d3f
Oliver Browne <oliver@ultyresults.com>
2018-12-18 10:56:03 +0000

    completed part 2 of the minesweeper task

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,32 +1,21 @@
-// this is minesweeper, a project part of the codecademy course on front-end dev
+// function that displays the current board..
+const printBoard = (board) => {
+  console.log('Current Board:');
+  console.log( board[0].join(' | ') );
+  console.log( board[1].join(' | ') );
+  console.log( board[2].join(' | ') );
+};
 
-// the blank line variable..
-const blankLine = '  |   |  ';
+// default board..
+let board = [ [' ',' ',' '],
+              [' ',' ',' '],
+              [' ',' ',' '] ];
 
-// simple demo of the most basic form (3 blank lines)...
-console.log(`This is what an empty board would look like:`);
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+// display the current board status
+printBoard(board);
 
-// setup a less simplified version with guesses and bombs..
-const guessLine = '1 |   |  ';
-const bombLine  = '  | B |  ';
-
-// print a version of it..
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
-
-// assign bomb to random row..
-let row = Math.floor(Math.random() * 3);
-console.log(row);
-console.log('This is what a board with a randomly located bomb on it would look like:');
-for (i=0; i<3; i++) {
-  if (i === row) {
-    console.log(bombLine)
-  } else {
-    console.log(blankLine)
-  }
-}
+// update manually..
+board[0][1] = '1';
+board[2][2] = 'b';
+// and redisplay..
+printBoard(board);
